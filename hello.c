6285f16fdc281f60b022f6a266f9437766e542ae
Chengwei <605074415@qq.com>
2020-04-02 19:37:23 +0800

    modify hello.c

--- hello.c
+++ hello.c
@@ -22,6 +22,8 @@
             int add_val = 0;
             add_val = add(0,0);
             printf("\n\rthe add_val is %d\r\n",add_val);
+			printf("hello world\r\n");
           
         }
+    
     
